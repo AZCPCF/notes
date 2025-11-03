441845043d60f79d436d384b8ea23ce0a1aa550e
Alisan <144158983+AZCPCF@users.noreply.github.com>
2025-11-03 15:44:42 +0330

    fix grids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const allTasks = await getAllNotes();
   console.log(allTasks);
   return (
-    <div className="grid grid-cols-4 gap-3">
+    <div className="grid grid-cols-4 gap-3 max-lg:grid-cols-2 max-md:grid-cols-1">
       {allTasks.map((t) => (
         <div
           key={t.id}
@@ -14,8 +14,18 @@ export default async function Home() {
           <h3 className="text-xl font-extrabold">{t.title}</h3>
           <p className="text-base truncate">{t.description}</p>
           <div className="flex gap-2 *:text-sm mt-2">
-            <Link href={"/add-note/" + t.id} className="bg-emerald-600 p-1 rounded-sm hover:bg-emerald-500 duration-100">update</Link>
-            <Link href={"/delete-note/" + t.id} className="bg-rose-600 p-1 rounded-sm hover:bg-rose-500 duration-100">delete</Link>
+            <Link
+              href={"/add-note/" + t.id}
+              className="bg-emerald-600 p-1 rounded-sm hover:bg-emerald-500 duration-100"
+            >
+              update
+            </Link>
+            <Link
+              href={"/delete-note/" + t.id}
+              className="bg-rose-600 p-1 rounded-sm hover:bg-rose-500 duration-100"
+            >
+              delete
+            </Link>
           </div>
         </div>
       ))}
